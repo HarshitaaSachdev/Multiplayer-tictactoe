7b3e1d1f610c62b5b012349734428ba386c25e4e
Harshita Sachdev <134152843+HarshitaaSachdev@users.noreply.github.com>
2024-06-16 00:19:10 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,63 @@
+import "./App.css";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import { StreamChat } from "stream-chat";
+import{Chat} from "stream-chat-react"
+import Cookies from "universal-cookie";
+import {useState}  from 'react';
+import JoinGame from "./components/JoinGame";
+
+function App() {
+  const api_key = "5yxfz56p97gr";
+  const cookies = new Cookies();
+  const token = cookies.get("token");
+  const client = StreamChat.getInstance(api_key);
+  const [isAuth, setIsAuth] = useState(false);
+
+  const logOut = () => {
+    cookies.remove("token");
+    cookies.remove("userId");
+    cookies.remove("firstName");
+    cookies.remove("lastName");
+    cookies.remove("hashedPassword");
+    cookies.remove("channelName");
+    cookies.remove("username");
+    client.disconnectUser();
+    setIsAuth(false);
+  }
+  if (token) {
+    client
+      .connectUser(
+        {
+          id: cookies.get("userId"),
+          name: cookies.get("username"),
+          firstName: cookies.get("firstName"),
+          lastName: cookies.get("lastName"),
+          hashedPassword: cookies.get("hashedPassword"),
+        },
+        token
+      )
+      .then((user) => {
+        setIsAuth(true);
+      });
+  }
+  return (
+    <div className="App">
+      {isAuth ? (
+        <Chat client={client}>
+          <JoinGame />
+          <div className="logoutbutton">
+          <button onClick={logOut}> Log Out</button>
+          </div>
+        </Chat>
+      ) : (
+        <>
+          <Signup setIsAuth={setIsAuth} />
+          <Login setIsAuth={setIsAuth} />
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Game.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import {useState}  from "react"
+import Board from './Board'
+import {Window, MessageList,MessageInput} from "stream-chat-react"
+import "./chat.css"
+
+function Game({channel,setChannel}) {
+
+    const [playersJoined,setPlayersJoined] = useState(channel.state.watcher_count === 2)
+
+    const [result, setResult] = useState({ winner: "none", state: "none" });
+
+    channel.on("user.watching.start",(event) => {
+        setPlayersJoined(event.watcher_count === 2)
+    })
+    if(!playersJoined){
+        return <div style={{color: 'black', fontStyle: "italic",fontSize: 20}}>Waiting for other player to join...</div>;
+    }
+  return <div className ="gameContainer">
+     <Board result={result} setResult={setResult} />
+     <Window>
+     <MessageList
+      disableDateSeparator
+      closeReactionSelectorOnClick
+      hideDeletedMessages
+      messageActions={["react"]}/>
+          <MessageInput noFiles/>
+     </Window>
+     <div className='leavegame'>
+     <button onClick={async()=>{
+       await channel.stopWatching();
+       setChannel(null);
+     }}>Leave Game</button>
+     </div>
+     {result.state === "won" && <div> {result.winner} Won The Game</div>}
+      {result.state === "tie" && <div> Game Tieds</div>}
+  </div>
+  
+}
+
+export default Game
